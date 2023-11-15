7ab1e34bb144eab77dd33a40917aa18590684613
Walmyr <wlsf82@gmail.com>
2023-11-16 00:23:59 +0100

    Remove non used stuff

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google'
 import { TestDashboard } from '../components/test-dashboard'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
